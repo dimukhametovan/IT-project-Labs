1d939f6e717e4f05665a59049ad20da21750f3f0
Popoou <anpopov2005@yandex.ru>
2025-05-16 21:22:12 +0300

    Добавлена таблица успеваемости

--- student-cabinet/src/pages/Login/Login.jsx
+++ student-cabinet/src/pages/Login/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
     setError('');
 
     try {
-      const response = await fetch('/auth/login', {
+      const response = await fetch('http://127.0.0.1:8000/docs/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
--- student-cabinet/src/pages/Register/Register.jsx
+++ student-cabinet/src/pages/Register/Register.jsx
@@ -31,7 +31,7 @@ const Register = () => {
     }
 
     try {
-      const response = await fetch('/auth/register', {
+      const response = await fetch('http://127.0.0.1:8000/docs/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
